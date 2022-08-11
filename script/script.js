be0e415855810f440646020d47367e0b2f4e0fa1
Denis Iurkin <denurkin@gmail.com>
2022-08-11 18:34:21 +0300

    One

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaprive;
+// let title,
+//     screens,
+//     screenPrice,
+//     rollback,
+//     fullPrice,
+//     adaprive;
 
-alert('привет');
-console.log('привет');
+// alert('привет');
+// console.log('привет');
+
+const title = 'HOMEWORK',
+      screens = "Простые, Сложные, Интерактивные",
+      screenPrice = 100,
+      rollback = 1,
+      fullPrice = 200000,
+      adaptive = true;
+
+console.log(typeof(title));
+console.log(typeof(adaptive));
+console.log(typeof(fullPrice));
+console.log(String(screenPrice).length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани” и “Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log(screens.toLocaleLowerCase().split());
+console.log(fullPrice * (rollback/100));
